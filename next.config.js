d0b442b8efcb2303d2cda7f4c8b4b0fa46130360
takumihashimoto0218 <shimayo02180218@gmail.com>
2024-12-22 04:15:56 +0900

    nextconfi

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 /** @type {import('next').Config} */
 
 module.exports = {
-	typescript: {
-		ignoreBuildErrors: true,
-	},
+  typescript: {
+    // !! 警告 !!
+    // あなたのプロジェクトに型エラーがあったとしても、プロダクションビルドを正常に完了するために危険な許可をする。
+    // !! 警告 !!
+    ignoreBuildErrors: true
+  }
 };
