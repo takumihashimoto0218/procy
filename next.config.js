95c395e5be67fcc100446b61da6a8149010893a0
takumihashimoto0218 <shimayo02180218@gmail.com>
2024-12-22 02:05:12 +0900

    nextconfig修正

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+/** @type {import('next').Config} */
+const config = {
+  // 設定内容
+};
+
+module.exports = config;
